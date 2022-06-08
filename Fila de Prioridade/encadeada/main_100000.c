ef18d006e5ba1043d0f5f5933ae771d1985ac24c
Rodrigo Monteiro do Nascimento <rodrigo.monteiro.nascimento@outlook.com>
2022-06-08 14:18:45 -0300

    init

---
+++ Fila de Prioridade/encadeada/main_100000.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+#include "fila_prioridade.h"
+
+int main(){
+
+    clock_t t;
+    t = clock(); 
+
+    int qtVezes = 100000;
+
+    for (int i = 0; i < qtVezes; i++)
+    {
+         FilaPrioridade* f = filaP_criar();
+
+         for (int j = 0; j < 100; j++)
+         {
+             filaP_inserir(f, j);  
+         }
+         
+         filaP_destruir(&f);
+    }
+
+    t = clock() - t; 
+    printf("\nTempo de execucao: %lf\n\n", ((double)t)/((CLOCKS_PER_SEC/1000))); 
+
+
+}
